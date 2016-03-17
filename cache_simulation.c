4cad7328c3683b99816503864defe35740b94937
seanDoJo <sedo8743@colorado.edu>
2016-03-17 11:58:59 -0600

    added main file for simulator

---
+++ cache_simulation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+//function declarations
+void cache_init(void);
+void cache_destroy(void);
+
+unsigned long long* cache;
+
+int main(int argc, char* argv[]){
+	//initialize cache with arguments
+
+}
+
+void cache_init(void){
+	//use parameters to malloc correct cache structure
+}
+
+void cache_destroy(void){
+	//function for freeing cache memory
+}
